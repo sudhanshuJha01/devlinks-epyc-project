bf72d1d39cbbaa8d7b295c68a19f8c09828db33f
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-10-20 02:24:03 +0530

    form created

---
+++ src/app/(main)/LinkForm/LinkForm.jsx
@@ -0,0 +1,75 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import optionData from "@/lib/config/optionData.js";
+
+const Page = () => {
+  const [selectedOption, setSelectedOption] = useState(optionData[0]?.value);
+
+  // Find the selected option's icon to display
+  const selectedPlatform = optionData.find(
+    (item) => item.value === selectedOption
+  );
+
+  return (
+    <section className="flex flex-col w-[728px] h-[228px] bg-[#FAFAFA] rounded-lg p-5 justify-center gap-3">
+      <section className="flex items-center justify-between text-[#737373]">
+        <section className="flex items-center justify-center gap-2">
+          <Image
+            src={"/images/icon-drag-and-drop.svg"}
+            alt="drag and drop icon"
+            width={12}
+            height={6}
+          />
+          <h2 className="font-bold text-base ">Link #1</h2>
+        </section>
+        <span className="text-base">Remove</span>
+      </section>
+
+      <form className="flex flex-col text-[#333333] text-xs gap-1">
+        <label htmlFor="platform-select">Platform</label>
+        <section className="flex items-center w-full bg-[#FFFFFF] border border-[#E0E0E0] focus:border-[#633CFF] focus:ring-[#633CFF] focus:ring-1 focus:ring-opacity-50 rounded-lg  pl-4 gap-3">
+          {/* Display the icon of the selected platform */}
+          {selectedPlatform && (
+            <Image
+            className=""
+              src={selectedPlatform.icon}
+              alt={selectedPlatform.name}
+              width={12.52}
+              height={14.52}
+            />
+          )}
+
+          <select
+            id="platform-select"
+            className="w-[100%]  py-3 outline-none  rounded-r-lg text-base"
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)} // Update selected option
+          >
+            {optionData?.map((item) => (
+              <option key={item?.value} value={item?.value}>
+                {item?.name}
+              </option>
+            ))}
+          </select>
+        </section>
+      </form>
+
+      <form className="flex flex-col text-[#333333] text-xs gap-1">
+        <label htmlFor="url">Link</label>
+        <input
+          className="px-4 py-3 rounded-lg bg-[#FFFFFF] border border-[#E0E0E0] focus:outline-none focus:border-[#633CFF] focus:ring-[#633CFF] focus:ring-1 focus:ring-opacity-50 stroke-[#D9D9D9]"
+          type="url"
+          name="url"
+          id="url"
+          placeholder="https://github.com/sudhanshuJha01"
+          pattern="https://.*"
+          size="30"
+          required
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Page;
---
+++ src/lib/config/optionData.js
@@ -0,0 +1,39 @@
+const optionData = [
+   {
+      name: "GitHub",
+      value: "github",
+      icon: "images/icon-github.svg",
+   },
+   {
+      name: "Twitter",
+      value: "twitter",
+      icon: "images/icon-twitter.svg",
+   },
+    {
+        name: "LinkedIn",
+        value: "linkedin",
+        icon: "images/icon-linkedin.svg",
+    },
+    {
+        name: "Facebook",
+        value: "facebook",
+        icon: "images/icon-facebook.svg",
+    },
+    {
+        name: "Stack Overflow",
+        value: "stack-overflow",
+        icon: "images/icon-stack-overflow.svg",
+    },
+{
+        name: "YouTube",
+        value: "youtube",
+        icon: "images/icon-youtube.svg",
+    },{
+        name: "Twitch",
+        value: "twitch",
+        icon: "images/icon-twitch.svg",
+    }
+    
+]
+
+export default optionData
